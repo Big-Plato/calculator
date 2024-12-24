b2aa9e316eed97e64aeb83c1479f40ee4ad4b322
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-24 13:44:58 -0300

    Begin to implement functionality

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const number = numbers.querySelectorAll('.number');
 const operators = container.querySelector('.operators');
 const operator = operators.querySelector('.operator');
 
+console.log(number)
+
+number.forEach(number => {
+    number.addEventListener("click", (e) => {
+        result.textContent += e.target.id;
+        console.log(e.target)
+    })
+})
+
+function operate (a, op, b) {
+    return "a op b";
+}
+
 function add (a, b) {
     return a + b;
 }
@@ -21,3 +34,4 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
